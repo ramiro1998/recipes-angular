e5f877951a8acac7b6b2e06447e7245704e88a54
Martín <alex.a.martin@accenture.com>
2023-11-21 10:27:23 -0300

    Get of ingredients list

--- recipes-angular/src/app/modules/ingredients/pages/ingredients-page/ingredients-page.component.ts
+++ recipes-angular/src/app/modules/ingredients/pages/ingredients-page/ingredients-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/core/models/recipe.model';
+import { RecipeService } from 'src/app/shared/services/recipe.service';
 
 @Component({
   selector: 'app-ingredients-page',
   templateUrl: './ingredients-page.component.html',
-  styleUrls: ['./ingredients-page.component.css']
+  styleUrls: ['./ingredients-page.component.css'],
 })
-export class IngredientsPageComponent {
+export class IngredientsPageComponent implements OnInit {
+  ingredientes: any[] = [];
 
+  constructor(private recipes: RecipeService) {}
+
+
+  ngOnInit(): void {
+    this.recipes.getAllRecips().subscribe((recipes: any) => {
+      console.log(recipes);
+      recipes.map((recipe: Recipe) => {
+        recipe.ingredients.map((ingredient: any) => {
+          this.ingredientes.push(ingredient);
+        });
+      })
+    });
+  }
 }
